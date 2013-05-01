ddaddf2e75fe4800eddf009243d68b41ef4dd18a
Dylan Hercher <dylan@generalassemb.ly>
2013-05-01 01:51:39 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trupoly::Application.config.secret_token = '5c79c8cf14bff958b5feb43cbd027aa8b1d7cd61fae7fea4c8d275ee70142d3e0417f8d303c4f31a808000bf353d6d97f849162a218a05a2fea25bda160ce7d9'
