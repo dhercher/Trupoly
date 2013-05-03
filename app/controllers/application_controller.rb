828b098694e493a27ebfe863d027cbe7b64e2043
Dylan Hercher <dylan@generalassemb.ly>
2013-05-03 15:06:30 -0400

    vf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   config.assets.enabled = true
   config.assets.version = '1.0'
-  # if defined?(Bundler)
-
-  #   # If you precompile assets before deploying to production, use this line
-  #   Bundler.require *Rails.groups(:assets => %w(development test))
-  #   # If you want your assets lazily compiled in production, use this line
-  #   # Bundler.require(:default, :assets, Rails.env)
-  # end
+  Bundler.require(:default, Rails.env) if defined?(Bundler)
 end
