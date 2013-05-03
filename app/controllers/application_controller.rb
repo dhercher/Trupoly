87625a01996c9d5cee697e171620e48a18bb5b71
Dylan Hercher <dylan@generalassemb.ly>
2013-05-03 15:08:11 -0400

    vf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  config.assets.enabled = true
-  config.assets.version = '1.0'
+  # config.assets.enabled = true
+  # config.assets.version = '1.0'
   Bundler.require(:default, Rails.env) if defined?(Bundler)
 end
